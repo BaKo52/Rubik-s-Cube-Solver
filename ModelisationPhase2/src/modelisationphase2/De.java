b96e198b150226466738daaa1ea03bed5fd24045
BaKo52 <74250655+BaKo52@users.noreply.github.com>
2021-11-30 15:44:47 +0100

    start

---
+++ ModelisationPhase2/src/modelisationphase2/De.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelisationphase2;
+
+/**
+ *
+ * @author bk296745
+ */
+public class De {
+    private int x;
+    private int y;
+    private int z;
+    
+    private boolean isEdge = false;
+    private boolean isCorner = false;
+}
